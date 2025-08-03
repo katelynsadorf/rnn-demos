240440403a071cd985a8fc428a0e8a200201aff0
kasadorf13 <ksadorf@caltech.edu>
2025-08-03 15:54:55 -0700

    Activation functions

--- character-text-generator/main.py
+++ character-text-generator/main.py
@@ -28,3 +28,13 @@ def one_hot(index, vocab_size):
 # Xavier Normalized Initialization
 def initWeights(input_size, output_size):
     return np.random.uniform(-1, 1, (input_size, output_size)) * np.sqrt(6 / (input_size + output_size))
+
+##### Activation Functions ######
+def tanh(input, derivative = False):
+    if derivative:
+        return 1 - (input ** 2)
+    
+    return np.tanh(input)
+
+def softmax(input):
+    return np.exp(input) / np.sum(np.exp(input))
